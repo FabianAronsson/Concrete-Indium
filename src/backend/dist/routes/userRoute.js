31b403d6ea9a8dde4f9db7360812e5af72a7e3ad
Rasmus Bengtsson <rasmusb229@gmail.com>
2020-11-26 10:56:00 +0100

    ✨Added userRoute scaffold in src/routes
    Running Windows 1909

---
+++ src/backend/dist/routes/userRoute.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+exports.userRoute = void 0;
+var express = require("express");
+function userRoute() {
+    var router = express.Router();
+    router.post("/", function (req, res) {
+        res.send("test send");
+    });
+    return router;
+}
+exports.userRoute = userRoute;
+//# sourceMappingURL=userRoute.js.map
