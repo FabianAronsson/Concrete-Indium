9914d6f5bb19de48a6204cad484f0dd1a7314a37
Rasmus Bengtsson <rasmusb229@gmail.com>
2020-11-26 14:48:55 +0100

    ♻ Prettied up some code

--- src/backend/src/interfaces/user.ts
+++ src/backend/src/interfaces/user.ts
@@ -1,22 +1,35 @@
-
+import * as bcrypt from "bcrypt";
 
 export default interface User {
+    //identifiers
+    id: string;
+    username: string;
     email: string;
+
+    //security
     passwordHash: string;
     salt: string;
 
     tokenGeneration: string;
 }
 
-export function createUser(email: string, password: string): User {
+export function createUser(email: string, password: string, username: string): User {
     //gen token
-    let _token:string = "";
-    
-    let _user:User = {email: email, 
-        passwordHash: password, 
-        tokenGeneration: _token,
-        salt: null }; // TODO; FIX SALT
+    let tokenGeneration:string = ""; //random token generation
+    let id: string = ""; //random id
+    let salt: string = ""; //salt
+    let passwordHash: string = "" + password;
 
+    let _user:User = 
+    {
+        id: id,
+        username: username,
+        email: email, 
+        passwordHash: passwordHash, 
+        tokenGeneration: tokenGeneration,
+        salt: salt 
+    }; // TODO; FIX SALT
 
     return _user;
+    
 }
