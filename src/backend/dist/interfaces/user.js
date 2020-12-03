759a0d0fcd2daf59c923d786dd1998e48da4696d
Mojtabarezaei4 <54796834+Mojtabarezaei4@users.noreply.github.com>
2020-12-03 13:10:26 +0100

    ✨ created authentication route handler
    Building windows 10 1909

--- src/backend/dist/interfaces/user.js
+++ src/backend/dist/interfaces/user.js
@@ -1,5 +1,5 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.createUser = void 0;
 function createUser(email, password, username) {
     var tokenGeneration = "";
--- src/backend/src/inversify.config.ts
+++ src/backend/src/inversify.config.ts
@@ -3,11 +3,14 @@ import { Container } from "inversify";
 
 import UserRouteInterface from "./interfaces/userRoute"
 import UserRoute from "./routes/userRoute";
+import AuthenticationRouteInterface from "./interfaces/authenticationRoute"
+import AuthenticationRoute from "./routes/authenticationRoute";
 
 
 let container = new Container({ defaultScope: "Singleton" });
 
 //container.bind<type>(TYPES.type).to(implimentation);
 container.bind<UserRouteInterface>(TYPES.UserRoute).to(UserRoute);
+container.bind<AuthenticationRouteInterface>(TYPES.AuthenticationRoute).to(AuthenticationRoute);
 
 export default container;
