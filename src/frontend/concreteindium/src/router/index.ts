e87e3fbcc5a27b20723c30af1f661be967056d48
Jeffreywolff <Jeffrey.wolff@elev.ga.ntig.se>
2020-12-03 13:29:45 +0100

    WIP🚧

--- src/frontend/concreteindium/src/router/index.ts
+++ src/frontend/concreteindium/src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/Home.vue';
+import Login from '../views/Login.vue';
 
 Vue.use(VueRouter)
 
@@ -11,13 +12,20 @@ const routes: Array<RouteConfig> = [
     component: Home
   },
   {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
     path: '/about',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  
+
 ]
 
 const router = new VueRouter({
---
+++ src/frontend/concreteindium/src/views/Login.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="formContainer">
+            <h1>hej</h1>
+        <form action="" method="post">
+            <label for="email">Email</label>
+            <input type="text"  id="email" name="email" placeholder="Yourname@domainName.com"><br>
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password">
+        </form>
+
+    </div>
+    
+</template>
+<style lang="scss">
+
+
+
+</style>
+
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Login extends Vue {}
+</script>
