5b746d10e47443e0093c2d4135d1e507c149684c
FabianAronsson <fabian.aronsson@elev.ga.ntig.se>
2020-12-03 20:02:29 +0100

    Changed some code.
    Running Windows 1909.

--- src/backend/src/modules/authenticationModule.ts
+++ src/backend/src/modules/authenticationModule.ts
@@ -1,6 +1,6 @@
 import {storeInput, connect, dispose} from './../dBmodule';
 import User from './../interfaces/user';
-import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken"; //error, no default export ???
 
 export default class AuthenticationModule {
 
@@ -23,9 +23,10 @@ export default class AuthenticationModule {
 
     ///
     createToken(user:User):string {
-        var TOKEN_SECRET = require('crypto').randomBytes(64).toString('hex');
-        var token = jwt.sign({ email: user.email }, TOKEN_SECRET, {expiresIn: '10m'} ) 
-        return token;
+        var signedToken = jwt.sign({ email: user.email },
+        'f6441bb8e8f656ad708ae43f355a0c8c59d2cef50567792286007ce410aa34ba999c974a293806485ebab9df15749146a419f2ffd824c888d2638e9caf313940' ,
+        { algorithm: 'RS256', expiresIn: '10m' } ) 
+        return signedToken;
     }
 
     
