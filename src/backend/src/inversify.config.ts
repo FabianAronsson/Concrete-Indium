a9626f28a9be573ebe8cd5441e896cf368dd2ebc
Fru1ty <pablo.jofre34@gmail.com>
2020-12-10 13:22:56 +0100

    added interface to types.ts & inversify.config.ts

--- src/backend/src/inversify.config.ts
+++ src/backend/src/inversify.config.ts
@@ -5,6 +5,8 @@ import UserRouteInterface from "./interfaces/userRoute"
 import UserRoute from "./routes/userRoute";
 import AuthenticationRouteInterface from "./interfaces/authenticationRoute"
 import AuthenticationRoute from "./routes/authenticationRoute";
+import ChatRouteInterface from "./interfaces/chatRoute"
+import ChatRoute from "./routes/chatRoute";
 
 
 let container = new Container({ defaultScope: "Singleton" });
@@ -12,5 +14,6 @@ let container = new Container({ defaultScope: "Singleton" });
 //container.bind<type>(TYPES.type).to(implimentation);
 container.bind<UserRouteInterface>(TYPES.UserRoute).to(UserRoute);
 container.bind<AuthenticationRouteInterface>(TYPES.AuthenticationRoute).to(AuthenticationRoute);
+container.bind<ChatRouteInterface>(TYPES.UserRoute).to(ChatRoute);
 
 export default container;
