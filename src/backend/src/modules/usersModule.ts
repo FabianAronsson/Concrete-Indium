466e8d0733194c29236f6e4c677def96d53a565e
Rasmus Bengtsson <rasmusb229@gmail.com>
2020-12-10 15:17:08 +0100

    [WIP]Added inverify di to usermodule

--- src/backend/src/modules/usersModule.ts
+++ src/backend/src/modules/usersModule.ts
@@ -1,35 +1,37 @@
 import * as mongoose from "mongoose";
 import user from "../interfaces/user";
 import userModule from "../interfaces/userModule";
+import { injectable, inject } from "inversify";
+import "reflect-metadata";
+
 
  
+const userSchema = new mongoose.Schema({
+  id: String,
+  username: String,
+  email: String,
+  passwordHash: String,
+  salt: String,
+  tokenGeneration: String
+});
+
+const User = mongoose.model('Person', userSchema);
+
+@injectable()
 export default class UserModule implements userModule{
-
-  constructor() {
-
-  const userSchema = new mongoose.Schema({
-    id: String,
-    username: String,
-    email: String,
-    passwordHash: String,
-    salt: String,
-    tokenGeneration: String
-  });
-
-  const User = mongoose.model('Person', userSchema);
-
-    createUser(username: String, email: String, password: String): void {
-    var newUser = new User({
-      username: username,
-      email: email,
-      password: password
-    }); 
-    return newUser
-  };
+   createUser(username: String, email: String, password: String): void {
+      var newUser = new User({
+        username: username,
+        email: email,
+        password: password
+      }) 
+      //replace with db module save
+   }
+  
+  
 
   getUser(email : String): user {
-    let user = User.findOne({email: email})
-    return user
+    let user = <unknown>User.findOne({email: email})
+    return <user>user;
     };
   }
-}
