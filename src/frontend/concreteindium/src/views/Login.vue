e823ec4d08423dd6db6f53d9b45b09ba8607c401
Jeffreywolff <Jeffrey.wolff@elev.ga.ntig.se>
2020-12-10 12:57:41 +0100

    Installed Particles.js and made form/basic login page
    I was trying to implement the particles.js library for a cool effect at the login page, but it shows it is a bit harder than expected, so for now I am putting that on ice.

--- src/frontend/concreteindium/src/views/Login.vue
+++ src/frontend/concreteindium/src/views/Login.vue
@@ -1,19 +1,21 @@
 <template>
-    <div class="mainLogin">
+    <div id="particles-js">
         <div class="formContainer">
                 <h1>Welcome</h1>
-            <form action="" method="post">
+            <form action="/login" method="post">
                 <label for="email">Email</label><br><br>
                 <input type="text"  id="email" name="email" placeholder="name@domain.com"><br><br><br>
                 <label for="password">Password</label><br><br>
-                <input type="password" id="password" name="password" placeholder="Secure password">
+                <input type="password" id="password" name="password" placeholder="Secure password"><br><br>
+                <input type="submit" value="Login" id="loginBtn">
             </form>
 
-            <button>Login</button>
         </div>
     </div>
-    
 </template>
+
+<script src="particles.js"></script>
+
 <style lang="scss">
 
 </style>
@@ -22,11 +24,20 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+// @ts-ignore
+import particles from 'particles.js';
 
 @Component({
   components: {
     HelloWorld,
   },
 })
-export default class Login extends Vue {}
+export default class Login extends Vue {
+    
+  mounted():void{
+    particles.load('particles-js', '../assets/particles.json', () =>  {
+      console.log('callback - particles-js config loaded');
+    });
+  }
+}
 </script>
