18c1d6a91ed1c278d0145eb87d412793790ce127
FabianAronsson <fabian.aronsson@elev.ga.ntig.se>
2020-12-10 13:38:40 +0100

    Skapade en hyfsat experimentell autentisering för lösenord.
    Det kommer kräva vissa andra moduler i t.ex. DBModule för att den ska fungera. Om BCrypt inte ska användas här så vet jag inte hur det ska genomföras.

--- src/backend/src/modules/authenticationModule.ts
+++ src/backend/src/modules/authenticationModule.ts
@@ -1,6 +1,7 @@
 import {storeInput, connect, dispose} from './../dBmodule';
 import User from './../interfaces/user';
 import * as jwt from "jsonwebtoken"; //error, no default export ???
+const bcrypt = require('bcrypt')
 
 export default class AuthenticationModule {
 
@@ -14,7 +15,7 @@ export default class AuthenticationModule {
     authenticateUser(token:string):boolean {
 
         var key = 'f6441bb8e8f656ad708ae43f355a0c8c59d2cef50567792286007ce410aa34ba999c974a293806485ebab9df15749146a419f2ffd824c888d2638e9caf313940'
-        jwt.verify(token, key, {algorithms: ['RS256', ]}, function(err, decoded) {
+        jwt.verify(token, key, {algorithms: ['RS256', /*other algorithms*/]}, function(err, decoded) {
             if(err){ //any error, expired or invalid or both
                 return false;
             }
@@ -22,15 +23,24 @@ export default class AuthenticationModule {
                 return true;
             }
           });
-          return false;
+          return false; // if for any reason, return false
     }
 
     authenticateUserWithPassword(email:string, password:string):boolean {
-
-        return false;
+        try{
+            /*let user = findOne(person in database)*/
+            let user = ""; //dummy variable
+            if(user){
+                let isPasswordCorrect:boolean = bcrypt.compare(email, password)
+                return isPasswordCorrect; 
+            }
+        }
+        catch{ //error, password or email is incorrect
+            console.log("Wrong password or email. Or user does not exist")
+            return false;
+        }
     }
 
-    ///
     createToken(user:User):string {
         var signedToken = jwt.sign({ email: user.email },
         'f6441bb8e8f656ad708ae43f355a0c8c59d2cef50567792286007ce410aa34ba999c974a293806485ebab9df15749146a419f2ffd824c888d2638e9caf313940' ,
