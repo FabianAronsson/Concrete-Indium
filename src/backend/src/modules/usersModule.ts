ccf5842eb7f18687cb1a278598bb9e2936513a04
Rasmus Bengtsson <rasmusb229@gmail.com>
2020-12-15 12:41:09 +0100

    Merged RBfrontAuth with development

---
+++ src/backend/src/modules/usersModule.ts
@@ -0,0 +1,39 @@
+import * as mongoose from "mongoose";
+import user from "../interfaces/user";
+import userModule from "../interfaces/userModule";
+import { injectable, inject } from "inversify";
+import "reflect-metadata";
+
+
+ 
+const userSchema = new mongoose.Schema({
+  id: String,
+  username: String,
+  email: String,
+  passwordHash: String,
+  salt: String,
+  tokenGeneration: String
+});
+
+const User = mongoose.model('Person', userSchema);
+
+@injectable()
+export default class UserModule implements userModule{
+   createUser(username: String, email: String, password: String): void {
+      var newUser = new User({
+        username: username,
+        email: email,
+        password: password
+      })
+
+      
+      //replace with db module save
+   }
+  
+  
+
+  getUser(email : String): user {
+    let user = <unknown>User.findOne({email: email})
+    return <user>user;
+    };
+  }
