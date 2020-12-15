853caf5c735b9dac436fb95f73410c3afc84109a
FabianAronsson <fabian.aronsson@elev.ga.ntig.se>
2020-12-15 10:51:59 +0100

    Lade till ett random id för user

--- src/backend/src/interfaces/user.ts
+++ src/backend/src/interfaces/user.ts
@@ -1,5 +1,6 @@
 import * as bcrypt from "bcrypt";
 import * as authModule from "./../modules/authenticationModule"
+import * as uuid from "uuid";
 
 export default interface User {
     //identifiers
@@ -17,7 +18,13 @@ export default interface User {
 export function createUser(email: string, password: string, username: string): User {
     //gen token
     let tokenGeneration:string = ""; //random token generation, from authModule
-    let id: string = ""; //random id
+    const v4options = 
+    {
+        random: [
+            Math.floor(Math.random() * Math.floor(255))
+        ],
+    };
+    let id: string = uuid.v4(v4options); //random id
     let salt: string = bcrypt.genSalt(10); //salt
     let passwordHash: string = bcrypt.hash(password, salt); //hashed password
 
@@ -29,7 +36,5 @@ export function createUser(email: string, password: string, username: string): U
         passwordHash: passwordHash, 
         tokenGeneration: tokenGeneration,
     }; 
-
     return _user;
-    
 }
