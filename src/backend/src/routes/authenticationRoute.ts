b560617449ed0080e952c9e6068f8b7003a837b7
Mojtabarezaei4 <54796834+Mojtabarezaei4@users.noreply.github.com>
2021-01-14 13:53:59 +0100

    Work in progress

--- src/backend/src/routes/authenticationRoute.ts
+++ src/backend/src/routes/authenticationRoute.ts
@@ -1,43 +1,44 @@
-require('dotenv').config()
-
 import AuthenticationRouteInterface from "./../interfaces/authenticationRoute";
 import * as express from "express";
 import { injectable, inject } from "inversify";
 import "reflect-metadata";
 import * as jwt from "jsonwebtoken";
+import authenticationModule from '../modules/authenticationModule';
+import types from '../types';
+
 
 @injectable()
 export default class AuthenticationRoute implements AuthenticationRouteInterface {
+    private _authenticationModule: authenticationModule;
+
+    public constructor(
+        @inject(types.AuthenticationModule) authenticationModule: authenticationModule
+    ) {
+        this._authenticationModule = authenticationModule;
+    }
+    
     authenticationRoute(): express.Router {
         let router = express.Router();
     
         router.post("/", (req, res) => {
-            // res.json(req.body);
-
             // Authenticate user- import from autenticationModule
+            this._authenticationModule.authenticateUser
+            
+            const email = req.body.email;
+            const user = { name: email };
 
-            // Import createToken from authenticationModule for create the token
+            const accessToken = this._authenticationModule.createToken;
 
-            const username = req.body.username;
-            const user = { name: username };
-
-            const accessToken = /*Import createToken from authenticationModule for create the token*/;
+            // Veryfing the token
+            const authHeader = req.headers['authorization']
+            const token = authHeader && authHeader.split(' ')[1]
+            if (token == null) return res.sendStatus(401)
+            this._authenticationModule.authenticateUserWithPassword
+            
             res.json({ accessToken: accessToken });
         });
         return router;
     }
 
-    authenticateToken(req: any, res: any, next: any){
-        const authHeader = req.headers['authorization'];
-        const token = authHeader && authHeader.split(' ')[1];
-        if (token == null) return res.sendStatus(401);
-
-        // Verify the token
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err: any, user: any) => {
-            if (err) return res.sendStatus(403);
-            req.user = user;
-            next();
-        });
-    }
 }
 
--- src/backend/src/types.ts
+++ src/backend/src/types.ts
@@ -1,11 +1,13 @@
 import UserRoute from "./interfaces/userRoute";
 import UserModule from "./interfaces/userModule"
+import AuthenticationModule from "./modules/authenticationModule";
 
 let TYPES = {
     UserRoute: Symbol("UserRoute"),
     AuthenticationRoute: Symbol("AuthenticationRoute"),
     ChatRoute: Symbol("ChatRoute"),
-    UserModule: Symbol("UserModule")
+    UserModule: Symbol("UserModule"),
+    AuthenticationModule: Symbol("AuthenticationModule")
 }
 
 export default TYPES;
