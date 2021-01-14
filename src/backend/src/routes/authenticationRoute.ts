e154eb6e2a29d2c27c33cac0ad754c2ff5167347
Mojtabarezaei4 <54796834+Mojtabarezaei4@users.noreply.github.com>
2021-01-14 13:12:39 +0100

    Working in Progress

--- src/backend/src/routes/authenticationRoute.ts
+++ src/backend/src/routes/authenticationRoute.ts
@@ -1,7 +1,10 @@
+require('dotenv').config()
+
 import AuthenticationRouteInterface from "./../interfaces/authenticationRoute";
 import * as express from "express";
 import { injectable, inject } from "inversify";
 import "reflect-metadata";
+import * as jwt from "jsonwebtoken";
 
 @injectable()
 export default class AuthenticationRoute implements AuthenticationRouteInterface {
@@ -10,10 +13,31 @@ export default class AuthenticationRoute implements AuthenticationRouteInterface
     
         router.post("/", (req, res) => {
             // res.json(req.body);
-            res.send("POST /authentication/");
+
+            // Authenticate user- import from autenticationModule
+
+            // Import createToken from authenticationModule for create the token
+
+            const username = req.body.username;
+            const user = { name: username };
+
+            const accessToken = /*Import createToken from authenticationModule for create the token*/;
+            res.json({ accessToken: accessToken });
         });
-    
         return router;
+    }
+
+    authenticateToken(req: any, res: any, next: any){
+        const authHeader = req.headers['authorization'];
+        const token = authHeader && authHeader.split(' ')[1];
+        if (token == null) return res.sendStatus(401);
+
+        // Verify the token
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err: any, user: any) => {
+            if (err) return res.sendStatus(403);
+            req.user = user;
+            next();
+        });
     }
 }
 
