33722b8669789dcf80d5e06dfcf3f5e1221a4208
Fru1ty <pablo.jofre34@gmail.com>
2021-01-19 09:50:19 +0100

    basic functionality done

--- src/frontend/concreteindium/src/views/LiveChat.vue
+++ src/frontend/concreteindium/src/views/LiveChat.vue
@@ -1,11 +1,80 @@
 <template>
-    <div class="livechat-grid-container">
-        <div class="server-info livechat-grid-item">server info</div>
-        <div class="server-scroll-list livechat-grid-item">server scroll list</div>
-        <div class="server-header livechat-grid-item">server header</div>
-        <div class="server-textfield livechat-grid-item">textfield</div>
-        <div class="server-textinput livechat-grid-item">input field</div>
+  <div class="main-livechat-container">
+    <div class="server-info-container">
+
+      <div class="server-info">
+        <h1>Lorem ipsum dolor sit MMM MMMMMM MMMM</h1>
+        <h2>sample text sample text sample text sample text</h2>
+      </div>
+
+      <div class="server-list-container">
+        <div class="server-list-item">example server</div>
+        <div class="server-list-item">example server</div>
+        <div class="server-list-item">example server</div>
+        <div class="server-list-item">example server</div>
+        <div class="server-list-item">example server</div>
+        <div class="server-list-item">example server</div>
+        <div class="server-list-item">example server</div>
+      </div>
     </div>
+
+    <div class="textfield-container">
+
+      <div class="textfield-header">
+        <img src="./../assets/fat_mario.gif" alt="fat_mario">
+        <div>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        </div>
+      </div>
+      
+      <div class="message-container">
+        <div class="message">
+          <img src="./../assets/fat_mario.gif" alt="fat_mario">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt odit ipsam magni deleniti</p>
+        </div>
+        <div class="message">
+          <img src="./../assets/fat_mario.gif" alt="fat_mario">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt odit ipsam magni deleniti dignissimos dolor numquam repellendus, fugiat, incidunt quam eligendi dolore maxime est at quas delectus id, cumque perferendis.</p>
+        </div>
+        <div class="message">
+          <img src="./../assets/fat_mario.gif" alt="fat_mario">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt odit ipsam magni deleniti dignissimos dolor numquam repellendus, fugiat, incidunt quam eligendi dolore maxime est at quas delectus id, cumque perferendis.</p>
+        </div>
+        <div class="message">
+          <img src="./../assets/fat_mario.gif" alt="fat_mario">
+          <p>Lorem ipsum dolor sit amet con, fugiat, incidunt quam eligendi dolore maxime est at quas delectus id, cumque perferendis.</p>
+        </div>
+        <div class="message">
+          <img src="./../assets/fat_mario.gif" alt="fat_mario">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt odit ipsam magni deleniti dignissimos dolor numquam repellendus, fugiat, incidunt quam eligendi dolore maxime est at quas delectus id, cumque perferendis.</p>
+        </div>
+        <div class="message">
+          <img src="./../assets/fat_mario.gif" alt="fat_mario">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt odit ipsam magni deleniti dignissimos dolor numquam repellendus, fugiat, incidunt quam eligendi dolore maxime est at quas delectus id, cumque perferendis.</p>
+        </div>
+        <div class="message">
+          <img src="./../assets/fat_mario.gif" alt="fat_mario">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt odit ipsam magni deleniti dignissimos dolor numquam repellendus, fugiat, incidunt quam eligendi dolore maxime est at quas delectus id, cumque perferendis.</p>
+        </div>
+        <div class="message">
+          <img src="./../assets/fat_mario.gif" alt="fat_mario">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt odit ipsam magni deleniti dignissimos dolor numquam repellendus, fugiat, incidunt quam eligendi dolore maxime est at quas delectus id, cumque perferendis.</p>
+        </div>
+        <div class="message">
+          <img src="./../assets/fat_mario.gif" alt="fat_mario">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt odit ipsam magni deleniti dignissimos dolor numquam repellendus, fugiat, incidunt quam eligendi dolore maxime est at quas delectus id, cumque perferendis.</p>
+        </div>
+      </div>
+
+
+      <div class="message-inputfield">
+        <form action="/livechat" method="POST">
+          <input type="text" name="message" id="message">
+          <input type="submit" value="Submit">
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
