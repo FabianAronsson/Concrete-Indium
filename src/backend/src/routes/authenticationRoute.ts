8c3761efa58546fb5b0988e0aff44597289ae5a4
Mojtabarezaei4 <54796834+Mojtabarezaei4@users.noreply.github.com>
2021-01-21 15:45:02 +0100

    Imp.ed the auth-route✨

--- src/backend/src/routes/authenticationRoute.ts
+++ src/backend/src/routes/authenticationRoute.ts
@@ -5,40 +5,49 @@ import "reflect-metadata";
 import * as jwt from "jsonwebtoken";
 import authenticationModule from '../modules/authenticationModule';
 import types from '../types';
+import userModule from '../interfaces/userModule';
+import UserModule from "../modules/usersModule";
 
 
 @injectable()
 export default class AuthenticationRoute implements AuthenticationRouteInterface {
     private _authenticationModule: authenticationModule;
+    private _userModule: userModule;
 
     public constructor(
-        @inject(types.AuthenticationModule) authenticationModule: authenticationModule
+        @inject(types.AuthenticationModule) authenticationModule: authenticationModule,
+        @inject(types.UserModule)userModule: userModule
     ) {
         this._authenticationModule = authenticationModule;
+        this._userModule = userModule
+
     }
+
+    
     
     authenticationRoute(): express.Router {
         let router = express.Router();
     
-        router.post("/", (req, res) => {
-            // Authenticate user- import from autenticationModule
-            this._authenticationModule.authenticateUser
-            
+        router.post("/", async (req, res) => {
+            // hämta användarnamn / lösenord
             const email = req.body.email;
-            const user = { name: email };
 
-            const accessToken = this._authenticationModule.createToken;
-
-            // Veryfing the token
-            const authHeader = req.headers['authorization']
-            const token = authHeader && authHeader.split(' ')[1]
-            if (token == null) return res.sendStatus(401)
-            this._authenticationModule.authenticateUserWithPassword
+            // hämta en kopia av användaren som dem representerar
+            let user = this._userModule.getUser(email);
             
-            res.json({ accessToken: accessToken });
+            // kolla att dem existerar(ifall inte retunera ett felmendellande)
+            if (await this._authenticationModule.authenticateUserWithPassword(email, req.body.password)) {
+            // skapa en token med den användaren
+                const accessToken = await this._authenticationModule.createToken(user);
+            // retunera resultatet
+                res.json({ accessToken: accessToken });
+            } else {
+                res.sendStatus(401);
+            }
         });
         return router;
     }
+
 
 }
 
