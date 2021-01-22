f7a9049d9698bb3ee944cb6221ce1bf236ca62f2
Jeffreywolff <Jeffrey.wolff@elev.ga.ntig.se>
2021-01-22 13:44:46 +0100

    Implemented types and LivechattModule
    Created constructor

--- src/backend/dist/routes/chatRoute.js
+++ src/backend/dist/routes/chatRoute.js
@@ -5,24 +5,35 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var inversify_1 = require("inversify");
 require("reflect-metadata");
+var types_1 = require("../types");
 var ChatRoute = (function () {
-    function ChatRoute() {
+    function ChatRoute(chatModule) {
+        this._chatModule = chatModule;
     }
     ChatRoute.prototype.chatRoute = function () {
         var router = express.Router();
-        router.post("/", function (req, res) {
-            res.send("POST /chat/");
-        });
-        return router;
+        router.post("/chat", function (req, res) {
+        }, private, _message, this._chatModule.NewchatMessage(req.body.username, req.body.timestamp, req.body.message));
+        res.send("POST /chat/");
     };
+    ;
     ChatRoute = __decorate([
-        inversify_1.injectable()
+        inversify_1.injectable(),
+        __param(0, inversify_1.inject(types_1.default.LiveChatModule)),
+        __metadata("design:paramtypes", [Object])
     ], ChatRoute);
     return ChatRoute;
 }());
 exports.default = ChatRoute;
+return router;
 //# sourceMappingURL=chatRoute.js.map
--- src/backend/src/routes/chatRoute.ts
+++ src/backend/src/routes/chatRoute.ts
@@ -2,18 +2,36 @@ import ChatRouteInterface from "./../interfaces/chatRoute";
 import * as express from "express";
 import { injectable, inject } from "inversify";
 import "reflect-metadata";
+import lCModule from "../interfaces/livechattmodule";
+import types from '../types';
 
 @injectable()
 export default class ChatRoute implements ChatRouteInterface {
-    chatRoute(): express.Router {
+    
+    constructor(@inject(types.LiveChatModule) chatModule: lCModule){
+        this._chatModule = chatModule;
+    }
+    
+    private _chatModule : lCModule;
+
+    public chatRoute(): express.Router {
+
+
         let router = express.Router();
     
-        router.post("/", (req, res) => {
+
+        router.post("/chat", (req, res) => {
+            
+
             // res.json(req.body);
             res.send("POST /chat/");
         });
     
         return router;
     }
+
+
 }
+
+
 
